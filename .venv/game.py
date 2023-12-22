ba9b750f6025db57a86218ecbda22515ae08c7fb
Andrey Shem <shemah@yandex.ru>
2023-12-22 08:49:48 +0300

    Git add games files

---
+++ .venv/game.py
@@ -0,0 +1,79 @@
+
+
+from gameparts import Board
+# Из файла exceptions.py, который лежит в пакете gameparts,
+# импортируется класс FieldIndexError.
+from gameparts.exceptions import FieldIndexError, CellOccupiedError
+
+
+
+def main():
+    game = Board()
+    # Первыми ходят крестики.
+    current_player = 'X'
+    # Это флаговая переменная. По умолчанию игра запущена и продолжается.
+    running = True
+    game.display()
+
+    # Тут запускается основной цикл игры.
+    while running:
+
+        print(f'Ход делают {current_player}')
+
+        # Запускается бесконечный цикл.
+        while True:
+            try:
+                row = int(input('Введите номер строки: '))
+                if row < 0 or row >= game.field_size:
+                    raise FieldIndexError
+                column = int(input('Введите номер столбца: '))
+                if column < 0 or column >= game.field_size:
+                    raise FieldIndexError
+                if game.board[row][column] != ' ':
+                    # Вот тут выбрасывается новое исключение.
+                    raise CellOccupiedError
+            except FieldIndexError:
+                print(
+                    'Значение должно быть неотрицательным и меньше '
+                    f'{game.field_size}.'
+                )
+                print('Введите значения для строки и столбца заново.')
+                continue
+            except CellOccupiedError:
+                print('Ячейка занята')
+                print('Введите другие координаты.')
+            except ValueError:
+                print('Буквы вводить нельзя. Только числа.')
+                print('Введите значения для строки и столбца заново.')
+                continue
+            except Exception as e:
+                print(f'Возникла ошибка: {e}')
+            else:
+                break
+
+        # Теперь для установки значения на поле само значение берётся
+        # из переменной current_player.
+        game.make_move(row, column, current_player)
+        game.display()
+
+        # После каждого хода надо делать проверку на победу и на ничью.
+        if game.check_win(current_player):
+            print(f'Победили {current_player}!')
+            running = False
+        elif game.is_board_full():
+            print('Ничья!')
+            running = False
+
+
+        # Тернарный оператор, через кототорый реализована смена игроков.
+        # Если current_player равен X, то новым значением будет O,
+        # иначе — новым значением будет X.
+
+        current_player = 'O' if current_player == 'X' else 'X'
+
+
+# А вот вызов этой функции.
+
+
+if __name__ == '__main__':
+    main()

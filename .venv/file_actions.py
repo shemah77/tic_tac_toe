335b5ac2b5152b27219bb3479834dfec1b64ea10
Andrey Shem <shemah@yandex.ru>
2023-12-22 14:59:11 +0300

    file_actions added

---
+++ .venv/file_actions.py
@@ -0,0 +1,8 @@
+# file_actions.py
+
+# Открыть на запись файл example.txt
+file = open('example.txt', 'w')
+# Записать в файл строку.
+file.write('Зевну, укроюсь с головою,\nбудильник заведу на март.\n')
+# Закрыть файл.
+file.close()
